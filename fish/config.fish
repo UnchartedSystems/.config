f4b7e9c3228131d0e14b007507214c6eb43292e4
Dan Whitaker <dan@uncharted.systems>
2023-09-11 10:49:52 -0600

    wm settings

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set fish_function_path $fish_function_path /home/dan/.workbench/plugin-foreign-env/functions
+
+fenv source ~/.profile
+
 starship init fish | source
